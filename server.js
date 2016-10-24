e387a07b1e8bcc2616c053f8cfeee52ba6a5e638
unknown <dhongadeaakash@gmail.com>
2016-10-25 04:12:38 +0530

    Added

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ var type = upload.single('userPDF')
 // }
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
@@ -99,7 +99,7 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
       mongoPassword = process.env[mongoServiceName + '_PASSWORD']
       mongoUser = process.env[mongoServiceName + '_USER'];
 
-  if (mongoHost && mongoPort && mongoDatabase) {
+  if (mongoHost && mongoPort && mongoDatabase) {  
     mongoURLLabel = mongoURL = 'mongodb://';
     if (mongoUser && mongoPassword) {
       mongoURL += mongoUser + ':' + mongoPassword + '@';
@@ -109,6 +109,9 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
     mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
 
   }
+}
+else{
+  mongoURL='mongodb://localhost/SFITabstract'
 }
 console.log("The service name is "+process.env.DATABASE_SERVICE_NAME)
 
@@ -167,7 +170,7 @@ app.use(function(err, req, res, next){
 
  
 app.listen(port);
-console.log('Server running on http://%s:%s',port);
+console.log('Server running on port',port);
 
 
 
